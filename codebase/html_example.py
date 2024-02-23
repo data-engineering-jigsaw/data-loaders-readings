cce8798b571cf66ecd207b0b1a2929a898e546b2
Jeff Katzy <jeffreyerickatz@gmail.com>
2024-02-22 16:58:22 -0800

    update html reader

--- codebase/html_example.py
+++ codebase/html_example.py
@@ -1,14 +1,19 @@
+import os
+
 from llama_index.core import VectorStoreIndex
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.readers.web import SimpleWebPageReader
 
-os.environ["OPENAI_API_KEY"] = ''
+os.environ["OPENAI_API_KEY"] = api_key
 
 documents = SimpleWebPageReader(html_to_text=True).load_data(
     ["http://paulgraham.com/worked.html"]
 )
+
 parser = SentenceSplitter(chunk_size=1024)
 nodes = parser.get_nodes_from_documents(documents)
-index = VectorStoreIndex(nodes)
-query_engine = index.as_query_engine()
-response = query_engine.query("What is this document about?")
+
+
+# index = VectorStoreIndex(nodes)
+# query_engine = index.as_query_engine()
+# response = query_engine.query("What is this document about?")
